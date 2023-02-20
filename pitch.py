bd7dd6aaaf331e30f71db7ced901f64bea98a959
Roberto Perrotti Filho - XMART SOLUTIONS <roberto.perrotti@xmartsolutions.com.br>
2023-02-20 18:45:57 -0300

    final version, for now

--- pitch.py
+++ pitch.py
@@ -7,8 +7,9 @@ import soundfile
 def main():
     input_file = input("Insira o nome da música de origem: ")
     #generates the output file based on the input_file name + _transposed.wav
-    output_file = input_file[:-4] + "_transposed.wav"
+    
     semitone = input("Insira o numero de semitons (números positivos sobem o tom, números negativos descem o tom): ")
+    output_file = input_file[:-4] + "_" + semitone + "-semitons_transposed.wav"
     semitone = int(semitone)
     y, sr = librosa.load(input_file, sr=44100)
     #new_y = librosa.effects.pitch_shift(y, sr, semitone, n_fft=16384)
